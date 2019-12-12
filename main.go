22df1475a1bed23e958fb0007e9198289300023c
lastshadowpl <xeloxpl@windowslive.com>
2019-12-12 22:42:10 +0100

    Add collapse-timeout

--- main.go
+++ main.go
@@ -29,7 +29,10 @@ func handleErr(err error) {
 
 func getWordsFromIndex(out chan string) {
 	baseLinkerURL := "https://dictionary.cambridge.org/browse/english/%s/"
-	linker := colly.NewCollector()
+	wordURLrx := "dictionary/english/.+$"
+	linker := colly.NewCollector(
+	// colly.Async(true),
+	)
 
 	// Links from a latter index to word chunks
 	linker.OnHTML("a.dil.tcbd", func(e *colly.HTMLElement) {
@@ -39,27 +42,32 @@ func getWordsFromIndex(out chan string) {
 	// Get links to the words
 	linker.OnHTML("a.tc-bd", func(e *colly.HTMLElement) {
 		isIdiom, _ := regexp.MatchString("idiom", e.ChildText("span.pos"))
+		isValid, _ := regexp.MatchString(wordURLrx, e.Attr("href"))
 
-		if !isIdiom {
+		if !isIdiom && isValid {
 			out <- e.Attr("href")
 		}
 	})
 
-	p := make([]byte, 26)
+	p := make([]byte, 1)
 	for i := range p {
-		toVisit := fmt.Sprintf(baseLinkerURL, string('a'+i))
+		toVisit := fmt.Sprintf(baseLinkerURL, string('z'+i))
 		linker.Visit(toVisit)
 	}
+
+	defer close(out)
 }
 
 func getWordsDefs(in chan string, out chan Word) {
 	c := colly.NewCollector(
 		colly.Async(true),
 	)
+	collapseTime := time.Second * 5
+	closeT := time.NewTimer(collapseTime)
 
 	c.OnHTML("article#page-content", func(article *colly.HTMLElement) {
 		word := Word{
-			word:    "",
+			word:    "?",
 			noun:    0,
 			adj:     0,
 			verb:    0,
@@ -75,6 +83,7 @@ func getWordsDefs(in chan string, out chan Word) {
 			word.word = headWord[0]
 		} else {
 			// Skip the word
+			out <- word
 			return
 		}
 
@@ -93,20 +102,32 @@ func getWordsDefs(in chan string, out chan Word) {
 			}
 		})
 
+		closeT = time.NewTimer(collapseTime)
 		out <- word
 	})
 
 	for link := range in {
 		c.Visit(link)
 	}
+
+	for alive := true; alive; {
+		select {
+		case <-closeT.C:
+			alive = false
+			close(out)
+		}
+	}
+
 }
 
 func main() {
 	start := time.Now()
 	links := make(chan string)
 	words := make(chan Word)
+	go getWordsFromIndex(links)
+	go getWordsDefs(links, words)
 
-	file, err := os.Create("out.csv")
+	file, err := os.Create("out-test.csv")
 	if err != nil {
 		panic(err)
 	}
@@ -114,17 +135,16 @@ func main() {
 	out := bufio.NewWriter(file)
 	defer file.Close()
 
-	go getWordsFromIndex(links)
-	go getWordsDefs(links, words)
-
 	out.WriteString("word,noun,adjective,verb,phrasal verb,adverb\n")
+
 	i := 0
-	repCache, err := lru.New(50)
+	missed := 0
+	repCache, err := lru.New(20)
 	handleErr(err)
 
 	for w := range words {
-		if !repCache.Contains(w.word) {
-			fmt.Printf("[%v] %v\n", i, w.word)
+		if !repCache.Contains(w.word) && w.word != "?" {
+			fmt.Printf("[%v; %v] %v\n", i, missed, w.word)
 
 			// !!! UPDATE THE ORDER OF PROPERTIES IN THE HEADER IF YOU CHANGE THEM HERE !!!
 			out.WriteString(fmt.Sprintf("%s,%d,%d,%d,%d,%d\n", w.word, w.noun, w.adj, w.verb, w.phrasal, w.adverb))
@@ -133,11 +153,11 @@ func main() {
 			if i%300 == 0 {
 				out.Flush()
 			}
-
-			if i == 4000 {
-				fmt.Println(time.Since(start))
-				return
-			}
+		} else if w.word == "?" {
+			missed++
 		}
 	}
+
+	out.Flush()
+	fmt.Println(time.Since(start))
 }
